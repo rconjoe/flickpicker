0aabefed8835ac232e7a9d87724b21ad4c312ccf
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-05-18 16:06:22 -0400

    #fix15: added route to update the vote count

--- Backend/routes.js
+++ Backend/routes.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.get('/search-movies', movieController.searchMovies);
 router.post('/save-movie', movieController.saveMovie);
 router.get('/movies', movieController.getMovies);
+router.post('/update-vote', movieController.updateVote);
 
 export default router;
