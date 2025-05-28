1fddfe396481137918daa61ce2c5a395405c9948
rconjoe <root@trog.codes>
2025-05-28 16:50:56 -0700

    move over some of the basic meta tags from old index.html

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,4 +1,8 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import {
+  HeadContent,
+  Outlet,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 import Header from '../components/Header'
@@ -11,9 +15,12 @@ interface MyRouterContext {
   queryClient: QueryClient
 }
 
+// this is the "entrypoint" in terms of the first thing to be rendered:
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
+      {/* <HeadContent> is necessary to render everything in the "head" property. */}
+      <HeadContent />
       <Header />
 
       <Outlet />
@@ -22,4 +29,30 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       <TanStackQueryLayout />
     </>
   ),
+  head: () => ({
+    // TODO: there is still a bunch of head/document stuff to move from the old index.html.
+    // https://tanstack.com/router/latest/docs/framework/react/guide/document-head-management
+    // The most important meta tags have been moved over already, as seen below.
+    meta: [
+      {
+        title: 'FlickPicker - Movie Night Tracking Application',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0',
+      },
+      {
+        name: 'description',
+        content: 'FlickPicker - Movie Night Tracking Application',
+      },
+      {
+        name: 'theme-color',
+        content: '#212529',
+      },
+      {
+        name: 'color-scheme',
+        content: 'light dark',
+      },
+    ],
+  }),
 })
