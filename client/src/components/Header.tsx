350df3afd9a4f96b25d7ba70b286841f4c25398e
rconjoe <root@trog.codes>
2025-05-28 19:46:55 -0700

    inserts logo in header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,9 @@ export default function Header() {
     <header className="p-2 flex gap-2 bg-white text-black justify-between">
       <nav className="flex flex-row">
         <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <img src="logo.svg" alt="FlickPicker Logo" width="30" height="24" />
+          </Link>
         </div>
 
         <div className="px-2 font-bold">
