90f23c35d243b4f2342725bdf4bc9282b1d13aa1
rconjoe <root@trog.codes>
2025-05-30 21:52:11 -0700

    ui: update header to contain flickpicker logo, search bar, and login dialog trigger

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,23 +1,29 @@
 import { Link } from '@tanstack/react-router'
+import Search from './Search'
+import Login from './Login'
 
 export default function Header() {
   return (
-    <header className="p-2 flex gap-2 bg-white text-black justify-between">
-      <nav className="flex flex-row">
+    <header className="p-2 flex gap-2 bg-secondary dark text-white justify-between items-center">
+      <nav className="flex flex-row items-center">
         <div className="px-2 font-bold">
           <Link to="/">
-            <img src="logo.svg" alt="FlickPicker Logo" width="30" height="24" />
+            <img src="logo.svg" alt="FlickPicker Logo" width="46" height="40" />
           </Link>
         </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/tanstack-query">TanStack Query</Link>
-        </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/store">Store</Link>
-        </div>
+        <Link to="/">
+          <span className="text-xl">FlickPicker</span>
+        </Link>
       </nav>
+
+      <div className="flex flex-row">
+        <div className="py-2 px-4 font-bold">
+          <Search />
+        </div>
+        <div className="py-2 px-4 font-bold">
+          <Login />
+        </div>
+      </div>
     </header>
   )
 }
