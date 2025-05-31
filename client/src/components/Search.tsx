1bad6732ffc4ce58ae7c2ddd4af6041495c1e2a1
rconjoe <root@trog.codes>
2025-05-30 21:53:07 -0700

    ui: init search component

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -5,21 +5,24 @@
  */
 import { FaSearch } from 'react-icons/fa'
 import { IconContext } from 'react-icons/lib'
+import { Button } from './ui/button'
 import { Input } from '@/components/ui/input'
 
 export default function Search() {
   return (
-    <div className="flex items-center w-full max-w-sm space-x-2 rounded-lg border border-gray-300 bg-gray-50 dark:bg-gray-900 px-3.5 py-2">
+    <div className="flex items-center w-full max-w-sm space-x-2 rounded-lg bg-secondary dark px-3.5 py-2">
       <Input
         type="search"
         placeholder="Search movies"
-        className="w-full border-0 h-8 font-semibold text-gray-600"
+        className="w-full border-0 h-8 font-semibold"
       />
-      <IconContext.Provider value={{ color: 'grey' }}>
-        <div>
-          <FaSearch />
-        </div>
-      </IconContext.Provider>
+      <Button type="submit" variant="ghost" onClick={() => console.log()}>
+        <IconContext.Provider value={{ color: 'grey' }}>
+          <div>
+            <FaSearch />
+          </div>
+        </IconContext.Provider>
+      </Button>
     </div>
   )
 }
